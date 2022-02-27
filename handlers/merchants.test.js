24f639ea31a3b457a91e306ec8b732805f2010ad
unknown <fiskahendiyaningsih@gmail.com>
2022-02-28 00:04:05 +0700

    update

--- handlers/merchants.test.js
+++ handlers/merchants.test.js
@@ -15,50 +15,50 @@ jest.mock("../storage", () => {
 
 
 // TES GET ID MERCHANT RESULT RESPONSE
-// test("getById returns an existing merchant", async () => {
-//   const request = httpMocks.createRequest({
-//     method: "GET",
-//     url: "/api/merchants/20014",
-//     params: {
-//       merchantId: 20014,
-//     },
-//   });
+test("getById returns an existing merchant", async () => {
+  const request = httpMocks.createRequest({
+    method: "GET",
+    url: "/api/merchants/20014",
+    params: {
+      merchantId: 20014,
+    },
+  });
 
-//   const response = httpMocks.createResponse();
-//   mockFindOneMerchant.mockResolvedValue({
-//     merchantId: 20014 ,
-//     name: "Toko Baru",
-//   });
+  const response = httpMocks.createResponse();
+  mockFindOneMerchant.mockResolvedValue({
+    merchantId: 20014 ,
+    name: "Toko Baru",
+  });
 
-//   await getById(request, response);
+  await getById(request, response);
 
-//   expect(response.statusCode).toEqual(200);
-//   expect(response._getJSONData()).toEqual({
-//     merchantId: 20014 ,
-//     name: "Toko Baru",
-//   });
-// });
+  expect(response.statusCode).toEqual(200);
+  expect(response._getJSONData()).toEqual({
+    merchantId: 20014 ,
+    name: "Toko Baru",
+  });
+});
 
 
-// // TEST 2 GET by ID merchant CASE WHEN ID MERCHANT FALSE OR DOESN'T EXIST
+// TEST 2 GET by ID merchant CASE WHEN ID MERCHANT FALSE OR DOESN'T EXIST
 
-// test("getById return 404 when a merchant id does not exist", async () => {
-//   const request = httpMocks.createRequest({
-//     method: "GET",
-//     url: "/api/merchants/2001",
-//     params: {
-//       merchantId: "200142",
-//     },
-//   });
+test("getById return 404 when a merchant id does not exist", async () => {
+  const request = httpMocks.createRequest({
+    method: "GET",
+    url: "/api/merchants/2001",
+    params: {
+      merchantId: "200142",
+    },
+  });
 
-//   const response = httpMocks.createResponse();
-//   mockFindOneMerchant.mockResolvedValue(null);
+  const response = httpMocks.createResponse();
+  mockFindOneMerchant.mockResolvedValue(null);
 
-//   await getById(request, response);
+  await getById(request, response);
 
-//   expect(response.statusCode).toEqual(404);
-//   expect(response._getData()).toEqual("404 - Not found");
-// });
+  expect(response.statusCode).toEqual(404);
+  expect(response._getData()).toEqual("404 - Not found");
+});
 
 
 // TEST 3 DELETE MERCHANT WITH PARAMS MerchantID
